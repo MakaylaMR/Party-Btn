980478e4d4f4054b08bf7b6c95dc792853126710
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-02-18 19:08:19 -0700

    changed everything

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
+//The button acts the same way as if it were a dark mode/light mode button except i wanted to add a little more color and fun to it
 'use strict';
 
 //variables
 const button = document.getElementById('button');
 const body = document.querySelector('body');
-//const nav is the id for the ul element... does this also affect all li child elements?
-const nav = document.getElementById('show');
+const p = document.querySelector('p');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+
 
 
 //Event
@@ -15,74 +24,26 @@ button.addEventListener('click', updateButton);
 //Button function
 //what does tony mean by state of toggle = on/off or open/closed?
 function updateButton() {
-  if (button.value === 'Dark Mode') {
-    button.value = 'Light Mode';
+  if (button.value === 'Party Mode') {
+    button.value = 'Normal Mode';
+    p.innerHTML = 'Click me for boring'
   } else {
-    button.value = 'Dark Mode';
+    button.value = 'Party Mode';
+    p.innerHTML = 'Click me for fun time';
   }
 }
 
-//Background-color for dark mode
+//Background-color for party mode
 function updateBackground() {
-    body.classList.toggle('dark-mode');
+  //background colour
+    body.classList.toggle('party-mode');
+  //svgs
+    one.classList.toggle('one');
+    two.classList.toggle('two');
+    three.classList.toggle('three');
+    four.classList.toggle('four');
+    five.classList.toggle('five');
+    six.classList.toggle('six');
+    seven.classList.toggle('seven');
+    eight.classList.toggle('eight');
 }
-
-//Event for hamburger menu
-//menubar.addEventListener('click', updateMenu);
-
-//hamburger menu
-function hamburgerMenu() {
-  nav.classList.toggle('no-show');
-}
-
-//random date list - array
-const date = [
-  ('<a href="http://kegsteakhouse.com/en/get-table">The Keg</a>'),
-  ('<a href="https://www.milestonesrestaurants.com/en/locations/5238/calgary-shaqanappi-trail-nw.html#">Milestones</a>'),
-  ('<a href="https://bostonpizza.com/en/reservation.html">Boston Pizza</a>'),
-  ('<a href="http://www.petersdrivein.com/"> Peters\' Drive-In</a>'),
-  ('<a href="https://unapizzeria.com/?pmo=AB"> Una Pizzeria</a>'),
-  ('<a href="https://www.redsdiner.com/"> Red\'s Diner</a>'),
-  ('<a href="https://famoso.ca/"> Famoso</a>')
-];
-
-const dessert = [
-  ('<a href="https://www.cravecupcakes.ca/">Crave Cupcakes</a>'),
-  ('<a href="https://www.dairyqueen.com/en-ca/locations/ab/calgary/2060-symons-valley-pky-nw/12997">Dairy Queen</a>'),
-  ('<a href="http://www.sweetreliefpastries.com/">Sweet Relief Pastries</a>'),
-  ('<a href="https://www.madebymarcus.ca/"> Made by Marcus</a>'),
-  ('<a href="https://www.byollia.com/">Ollia Macarons & Tea</a>')
-];
-
-const activity = [
-  ('<a href="https://www.cineplex.com/Theatres/Location/Calgary">Cineplex</a>'),
-  ('<a href="https://www.calgary.ca/csps/parks/locations/outdoor-skating-rinks.html">Skating</a>'),
-  ('<a href="https://www.calgaryzoo.com/">Zoo</a>'),
-  ('<a href="https://www.glenbow.org/">Glenbow Museum</a>'),
-  ('<a href="http://www.vingogh.ca/"> VinGogh</a>')
-];
-
-//assigning variables
-const dinner = document.getElementById('dinner');
-const randomDin = document.getElementById('randomDin');
-const randomDes = document.getElementById('randomDes');
-const randomAct = document.getElementById('randomAct');
-let randAct;
-let randDes;
-let randDate;
-
-//making it so the button outputs result
-function updateDinner(){
-  if (dinner.value === 'Choose my date!') {
-    //this is for the dinner array
-   randDate = date[Math.floor(Math.random() * date.length)];
-    //this is for the dessert array
-   randDes = dessert[Math.floor(Math.random() * dessert.length)];
-    //this is for the activity array
-    randAct = activity[Math.floor(Math.random() * activity.length)];
-   }
-  //this is for the paragraph tags
-  randomAct.innerHTML = randAct;
-  randomDin.innerHTML = randDate;
-  randomDes.innerHTML = randDes;
- }
